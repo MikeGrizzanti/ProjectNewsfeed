83c03c87c26c80a7b5ae0c8ea5b7c791dd103e5c
Maxi Penduzzu <maximilian.sudo@gmail.com>
2016-11-21 12:00:02 +0100

    updated db-script and design

--- js/feedparser.js
+++ js/feedparser.js
@@ -1,1 +1,12 @@
 'use strict';
+
+$.get('http://www.bild.de/rssfeeds/vw-home/vw-home-16725562,sort=1,view=rss2.bild.xml', function (data) {
+    $(data).find("entry").each(function () { // or "item" or whatever suits your feed
+        var el = $(this);
+
+        console.log("------------------------");
+        console.log("title      : " + el.find("title").text());
+        console.log("author     : " + el.find("author").text());
+        console.log("description: " + el.find("description").text());
+    });
+});
