6f8e7e723785d96b95ce1870bf12286dd6c26486
Mike Grizzanti <mike.grizzanti@gmail.com>
2016-12-05 11:05:25 +0100

    register.inc.php changes

--- models/register.inc.php
+++ models/register.inc.php
@@ -9,13 +9,13 @@ $user_repeat_password = "";
 $error = "";
 
 if ($_POST) {
-    if (isset($_POST['user_firstName']) && isset($_POST['user_lastName']) && isset($_POST['user_nickName']) && isset($_POST['user_password']) && isset($_POST['user_repeat_password']) && isset($_POST['user_eMail'])) {
-        $user_firstName = trim($_POST['user_firstName']);
-        $user_lastName = trim($_POST['user_lastName']);
-        $user_nickName = trim($_POST['user_nickName']);
-        $user_password = trim($_POST['user_password']);
-        $user_repeat_password = trim($_POST['user_repeat_password']);
-        $user_eMail = trim($_POST['user_eMail']);
+    if (isset($_POST['text_field_reg_first_name']) && isset($_POST['text_field_reg_last_name']) && isset($_POST['text_field_reg_nick_name']) && isset($_POST['text_field_reg_password']) && isset($_POST['text_field_reg_rep_password']) && isset($_POST['text_field_reg_email'])) {
+        $user_firstName = trim($_POST['text_field_reg_first_name']);
+        $user_lastName = trim($_POST['text_field_reg_last_name']);
+        $user_nickName = trim($_POST['text_field_reg_nick_name']);
+        $user_password = trim($_POST['text_field_reg_password']);
+        $user_repeat_password = trim($_POST['text_field_reg_rep_password']);
+        $user_eMail = trim($_POST['text_field_reg_email']);
 
         if (empty($user_firstName) || empty($user_lastName) || empty($user_nickName) || empty($user_password) || empty($user_repeat_password) || empty($user_eMail)) {
             $error = "Please fill all fields";
