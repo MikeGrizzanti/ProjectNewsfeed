34d20446cbad224e66c387e0b95c601a06291a40
Mike Grizzanti <mike.grizzanti@gmail.com>
2016-12-05 09:32:23 +0100

    controller created

--- controller/controller.php
+++ controller/controller.php
@@ -1,8 +1,52 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'models/functions.inc.php';
+
+session_start();
+
+class controller {
+    
+    private $context = array();
+
+    public function run($action){
+        $this->$action();
+        $this->generatePage($action);
+    }
+    
+    public function login(){
+        $erg = is_loggedIn();
+        if(!$erg){
+            require_once 'models/login.inc.php';
+            $this->addContext("error", $error);
+        } 
+        else 
+            header("Location:index.php?action=main");
+    }
+    
+    public function logout() {
+        session_destroy();
+        header("Location:index.php");
+    }
+    
+    public function main() {
+        $erg = is_loggedIn();
+        if(!$erg){
+            header("Location:index.php");
+        } 
+    }
+    
+    public function register() {
+        require_once 'models/register.inc.php';
+        $this->addContext("error", $error);
+    }
+    
+    private function generatePage($template){
+        extract($this->context);
+        require_once 'view/'.$template.".tpl.html";
+    }
+    
+    private function addContext($key, $value){
+        $this->context[$key] = $value;
+    }
+}
 
