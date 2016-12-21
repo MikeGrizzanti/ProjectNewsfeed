86d8ee4cddbef3b506a3f3838e46a3c012ba5d88
Maxi Penduzzu <maximilian.sudo@gmail.com>
2016-12-21 15:14:15 +0100

    test

--- js/css/transitions.js
+++ js/css/transitions.js
@@ -1,9 +1,9 @@
 alert("here");
  
 $(document).ready(function() {
+    
     //Hamburger Open&Close
     $(".top_icon_open_sidebar").click(function () {
-        alert("here");
-        //$(this).toggleClass("top_icon_close_sidebar");
+        $(this).toggleClass("top_icon_close_sidebar");
     });
 });
