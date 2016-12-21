c812224142db613bb59fb4a995c74955cb497262
Maxi Penduzzu <maximilian.sudo@gmail.com>
2016-12-21 15:06:13 +0100

    js_change

--- js/css/transitions.js
+++ js/css/transitions.js
@@ -1,13 +1,8 @@
-"use strict";
-
+alert("here");
+ 
 
 //Hamburger Open&Close
 $(".top_icon_open_sidebar").click(function () {
     alert("here");
     //$(this).toggleClass("top_icon_close_sidebar");
 });
-
-
-/*<script type="text/javascript"> $("#register_option").click(function() {
-                                    $(".flipper").css("transform","rotateY(180deg)");});
-                        </script>*/
