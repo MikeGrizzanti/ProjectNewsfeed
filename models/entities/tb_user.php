245dcc59bc02908c13eb3f996ba285e0721a795f
MikeGrizzanti <mike.grizzanti@gmail.com>
2016-12-21 14:26:37 +0100

    df

--- models/entities/tb_user.php
+++ models/entities/tb_user.php
@@ -125,12 +125,12 @@ class tb_user {
     }
 
     private static function createUser($firstName, $lastName, $nickName, $password, $eMail) {
-        $sqlcheck = "SELECT * FROM tb_user where user_nickName = '?' AND user_eMail = '?';";
+        $sqlcheck = "SELECT * FROM tb_user where user_nickName = ? AND user_eMail = ?;";
         $queryCheck = DB::getDB()->prepare($sqlcheck);
         $queryCheck->execute(array($nickName,$eMail));
-        $queryCheck->setFetchMode(PDO::FETCH_CLASS, 'tb_user');
+        //$queryCheck->setFetchMode(PDO::FETCH_CLASS, 'tb_user');
         
-        var_dump($queryCheck->fetchAll());
+        var_dump($queryCheck->fetch());
         
         if($queryCheck->rowCount() > 0) {
             return NULL;
