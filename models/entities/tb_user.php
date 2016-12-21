f4b6a99cae69eccca3838df17f4c46a947497cb8
MikeGrizzanti <mike.grizzanti@gmail.com>
2016-12-21 14:10:01 +0100

    df

--- models/entities/tb_user.php
+++ models/entities/tb_user.php
@@ -116,10 +116,21 @@ class tb_user {
         //!!!! TODO
     }
     
+    private static function checkIfÚnique ($nickName, $eMail) {
+        $sqlcheck = 'SELECT * FROM tb_user where user_nickName = ? AND user_eMail = ?;';
+        $queryCheck = DB::getDB()->prepare($sqlcheck);
+        $queryCheck->execute(array($nickName,$eMail));
+        
+        var_dump($queryCheck);
+    }
+
     private static function createUser($firstName, $lastName, $nickName, $password, $eMail) {
         $sqlcheck = 'SELECT * FROM tb_user where user_nickName = ? AND user_eMail = ?;';
         $queryCheck = DB::getDB()->prepare($sqlcheck);
         $queryCheck->execute(array($nickName,$eMail));
+        $queryCheck->setFetchMode(PDO::FETCH_CLASS, 'tb_user');
+        
+        var_dump($queryCheck->fetch());
         
         if($queryCheck->rowCount() > 0) {
             return NULL;
