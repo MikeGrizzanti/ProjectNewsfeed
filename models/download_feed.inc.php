b2b907b6cd44eefff8499e0f1a3be9cf0e3ffc41
MaxiPenduzzu <maximilian.sudo@gmail.com>
2016-12-28 21:35:11 +0100

    new download files

--- models/download_feed.inc.php
+++ models/download_feed.inc.php
@@ -4,7 +4,7 @@ $testName = "Tester.xml";
 
 //curl and header
 $ch = curl_init();
-$source = "http://www.ilfattoquotidiano.it/rss";
+$source = "http://www.spiegel.de/politik/index.rss";
 $headers = get_headers($source, 1);
 
 //curl setup
@@ -13,41 +13,38 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec ($ch);
 curl_close ($ch);
 
+
+//name
+$file_name = preg_replace('#^https?://#', '', $source);
+parse_url($file_name);
+echo array(3)["host"];
+
+
 //destination setup
 $destination = "xml_downloads/" . $testName;
 
 
-/*
-$feed = new SimplePie();
-$feed->set_feed_url('http://example.com/rss');
-$feed->init();
-$feed->handle_content_type();
-
-if ($feed->error())
-{
-    // this feed has errors
-}
-*/
-
 //XMLReader setup
-$dom = new DOMDocument;
-$dom->Load($source);
-if ($dom->validate()) {
-    echo "This document is valid!\n";
-}
+$validator = "http://validator.w3.org/feed/check.cgi?url=".$source."&output=soap12";
 
+$response = file_get_contents($validator);
+$a = strpos($response, '<m:validity>', 0)+12; 
+$b = strpos($response, '</m:validity>', $a); 
+$result = substr($response, $a, $b-$a);
 
 // URL is valid
-if ($headers[0] == 'HTTP/1.1 200 OK') {
-    //test it
-    print_r(get_headers($source, 1));
-    
-    //exec
-    /*$file = fopen($destination, "w+");
-    fputs($file, $data);
-    fclose($file);*/
+if ($headers[0] == 'HTTP/1.1 200 OK' && $result == 'true') {
+        //test it
+        print_r(get_headers($source, 1));
 
+        //exec
+        $file = fopen($destination, "w+");
+        fputs($file, $data);
+        fclose($file);
 } 
 elseif ($headers[0] == 'HTTP/1.1 404 Not Found'){
     print_r("Error! This url cannot be retrived: Server response -> " .$headers[0]); 
+}
+else{
+    print_r("We're sorry this feed could not be processed!"); 
 }
