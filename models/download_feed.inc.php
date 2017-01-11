79b744e06cd00b6355bf66c718ea46f29f1873f8
MikeGrizzanti <mike.grizzanti@gmail.com>
2017-01-11 14:37:56 +0100

    vfc

--- models/download_feed.inc.php
+++ models/download_feed.inc.php
@@ -1,45 +1,46 @@
 <?php
+if ($_POST) {
+    //curl and header
+    $ch = curl_init();
+    if (isset($_POST('add_feed'))) {
+       $source = trim($_POST['add_feed']);
+    }
+    $headers = get_headers($source, 1);
 
-//curl and header
-$ch = curl_init();
-if (isset($_POST('add_feed'))) {
-   $source = trim($_POST['add_feed']);
-}
-$headers = get_headers($source, 1);
-
-//curl setup
-curl_setopt($ch, CURLOPT_URL, $source);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$data = curl_exec ($ch);
-curl_close ($ch);
-
-
-//name
-$file_name = preg_replace('#^https?://www#', '', $source);
-$parsed_url = parse_url($source, PHP_URL_HOST);
-
-//destination setup
-$destination = "xml_downloads/" . $parsed_url;
+    //curl setup
+    curl_setopt($ch, CURLOPT_URL, $source);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec ($ch);
+    curl_close ($ch);
 
 
-//XMLReader setup
-$validator = "http://validator.w3.org/feed/check.cgi?url=".$source."&output=soap12";
+    //name
+    $file_name = preg_replace('#^https?://www#', '', $source);
+    $parsed_url = parse_url($source, PHP_URL_HOST);
 
-$response = file_get_contents($validator);
-$a = strpos($response, '<m:validity>', 0)+12; 
-$b = strpos($response, '</m:validity>', $a); 
-$result = substr($response, $a, $b-$a);
+    //destination setup
+    $destination = "xml_downloads/" . $parsed_url;
 
-// URL is valid
-if ($headers[0] == 'HTTP/1.1 200 OK' && $result == 'true') {
-        //exec
-        $file = fopen($destination, "w+");
-        fputs($file, $data);
-        fclose($file);
-} 
-elseif ($headers[0] == 'HTTP/1.1 404 Not Found'){
-    print_r("Error! This url cannot be retrived: Server response -> " .$headers[0]); 
-}
-else{
-    print_r("We're sorry this feed could not be processed!"); 
+
+    //XMLReader setup
+    $validator = "http://validator.w3.org/feed/check.cgi?url=".$source."&output=soap12";
+
+    $response = file_get_contents($validator);
+    $a = strpos($response, '<m:validity>', 0)+12; 
+    $b = strpos($response, '</m:validity>', $a); 
+    $result = substr($response, $a, $b-$a);
+
+    // URL is valid
+    if ($headers[0] == 'HTTP/1.1 200 OK' && $result == 'true') {
+            //exec
+            $file = fopen($destination, "w+");
+            fputs($file, $data);
+            fclose($file);
+    } 
+    elseif ($headers[0] == 'HTTP/1.1 404 Not Found'){
+        print_r("Error! This url cannot be retrived: Server response -> " .$headers[0]); 
+    }
+    else{
+        print_r("We're sorry this feed could not be processed!"); 
+    }
 }
