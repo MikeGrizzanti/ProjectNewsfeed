664eccac3af50861044d37a15c4746d353c87dda
MaxiPenduzzu <maximilian.sudo@gmail.com>
2017-01-18 16:16:10 +0100

    dffad

--- models/download_feed.inc.php
+++ models/download_feed.inc.php
@@ -35,7 +35,7 @@ if ($_POST) {
                 $xml = simplexml_load_string($xml);
                 $json = json_encode($xml);
                 $array = json_decode($json,TRUE);
-                print_r($array[0]);
+                print_r($array);
     
 
         // URL validations based on $retcode
