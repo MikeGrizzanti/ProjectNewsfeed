027faaf7b749c17723d954bad3252063942aa2f0
Maxi Penduzzu <maximilian.sudo@gmail.com>
2017-01-23 08:49:23 +0100

    downloads

--- js/css/transitions.js
+++ js/css/transitions.js
@@ -1,6 +1,16 @@
  $(document).ready(function() {
     
-    //Hamburger Open&Close
+    //rotate login_div REGISTER
+    $("#register_option").click(function() {
+        $(".flipper").css("transform","rotateY(180deg)");
+    });
+     
+    //rotate login_div REGISTER -> Back
+     $("#switch_to_login").click(function() {
+        $(".flipper").css("transform","rotateY(360deg)");
+     });
+     
+     //Hamburger Open&Close
     $(".top_icon_open_sidebar").click(function () {
         $(this).toggleClass("Hide_Hamburger");
         $(".top_icon_close_sidebar").toggleClass("Show_Cross");
@@ -11,5 +21,6 @@
      //Open add feed
     $(".add_feed").click(function () {
         $(".url_box").toggleClass("url_box_show");
-    });
+    }); 
+     
 });
