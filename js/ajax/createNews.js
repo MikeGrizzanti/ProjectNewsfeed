fd75c395cf53c9e4042afb6a753d8dd106a01559
MaxiPenduzzu <maximilian.sudo@gmail.com>
2017-01-23 21:36:06 +0100

    dffad

--- js/ajax/createNews.js
+++ js/ajax/createNews.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
    $.ajax({
     url:"/models/download_feed.inc.php",  
-    success:function(data) {
+    success:function(data) { //ok
       console.log(data); 
     },
-    error: function (xhr, ajaxOptions, thrownError) {
+    error: function (xhr, ajaxOptions, thrownError) { //error
         alert(xhr.status);
         alert(thrownError);
       }
