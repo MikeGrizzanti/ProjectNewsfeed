71a3c9eb9e651085e7cc3fb17fd16a36b13e6b3a
MaxiPenduzzu <maximilian.sudo@gmail.com>
2017-01-25 20:31:36 +0100

    dffad

--- js/ajax/createNews.js
+++ js/ajax/createNews.js
@@ -1,16 +1,20 @@
+var data_irr = 'irrelevant';
+        
+
 $(document).ready(function() {
     
     
-    $("#add_feed_box").submit(function(){        
-        var data = $('.url_box url_box_show').serialize(); // blank the input
+    //$("#add_feed_box").submit(function(){        
+        //var data_irr = 'irrelevant';
         
         $.ajax({
             type: "POST",
             url:"/models/download_feed.inc.php",
-            data: data,
+            data: {mydata: data_irr},
             //dataType: 'json',
-            success:function(data) { //ok
-                console.log(data); 
+            success: function(data){
+               console.log('worky');
+               alert(data); // It should now, worky!
             },
             error: function (xhr, ajaxOptions, thrownError, url) { //error
                 console.log(xhr.status);
@@ -18,5 +22,5 @@ $(document).ready(function() {
                 console.warn(thrownError);
             }
         }); 
-    });
+    //});
 });
