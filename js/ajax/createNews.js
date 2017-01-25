0462a03a21ea0ec639344e531eaae54895f6bfa5
Maxi Penduzzu <maximilian.sudo@gmail.com>
2017-01-25 15:19:11 +0100

    testing ajax connection

--- js/ajax/createNews.js
+++ js/ajax/createNews.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
    
     
-    $("#add_feed_box").submit(function(e){        
-        e.preventDefault(); // don't submit multiple times
-        this.submit(); // use the native submit method of the form element
+    $("#add_feed_box").submit(function(){        
         var data = $('.url_box').val(); // blank the input
     
         
         $.ajax({
-        type: "POST",
-        url:"/models/download_feed.inc.php",
-        data: data,
-        //dataType: 'json',
-        success:function(data) { //ok
-          console.log(data); 
-        },
-        error: function (xhr, ajaxOptions, thrownError, url) { //error
-            console.log(xhr.status);
-            console.log(url);
-            console.warn(thrownError);
-          }
+            type: "POST",
+            url:"/models/download_feed.inc.php",
+            data: data,
+            //dataType: 'json',
+            success:function(data) { //ok
+              ret_data =data;
+                console.log(data); 
+            },
+            error: function (xhr, ajaxOptions, thrownError, url) { //error
+                console.log(xhr.status);
+                console.log(url);
+                console.warn(thrownError);
+            }
         }); 
-        
     });
-       
+       console.log(ret_data);
 });
