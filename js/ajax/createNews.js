94c87b334f4060675b071ff1b035ddf54bec745c
Maxi Penduzzu <maximilian.sudo@gmail.com>
2017-01-25 14:43:33 +0100

    testing ajax connection

--- js/ajax/createNews.js
+++ js/ajax/createNews.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
    
     var data = $('.url_box').serialize();
-    
-    $.ajax({
-    type: "POST",
-    url:"/models/download_feed.inc.php",
-    data: data,
-    dataType: 'json',
-    success:function(data) { //ok
-      console.log(data); 
-    },
-    error: function (xhr, ajaxOptions, thrownError) { //error
-        alert(xhr.status);
-        console.warn(thrownError);
-      }
+    $(".url_box").submit(function(){
+       $.ajax({
+        type: "POST",
+        url:"/models/download_feed.inc.php",
+        data: data,
+        dataType: 'json',
+        success:function(data) { //ok
+          console.log(data); 
+        },
+        error: function (xhr, ajaxOptions, thrownError) { //error
+            alert(xhr.status);
+            console.warn(thrownError);
+          }
+        }); 
     });
   });
