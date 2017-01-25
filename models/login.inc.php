f64eaf573803bf89aec5c4e484db5f5c61ccdc5e
MikeGrizzanti <mike.grizzanti@gmail.com>
2017-01-25 13:40:12 +0100

    a

--- models/login.inc.php
+++ models/login.inc.php
@@ -23,3 +23,18 @@ if ($_POST) {
     }
 }
 
+
+//Facebook-Login
+$fb = new Facebook\Facebook([
+  'app_id' => '167575093726927', 
+  'app_secret' => '{app-secret}',
+  'default_graph_version' => 'v2.2',
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+$permissions = ['email']; // Optional permissions
+$loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+
