baae8d04420cba6a679c8ca54eb3ddcb716ae3a2
Maxi Penduzzu <maximilian.sudo@gmail.com>
2017-02-08 13:43:55 +0100

    gfsfdg

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require_once 'models/entities/tb_groupChatPartecipants.php';
 require_once 'models/entities/tb_message.php';
 require_once 'models/entities/tb_user.php';
 require_once 'models/entities/tb_user_interests.php';
-require_once 'models/download_feed.inc.php';
 
 require_once 'controller/controller.php';
 
