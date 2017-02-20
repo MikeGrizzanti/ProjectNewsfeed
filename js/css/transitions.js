4ce3d4ec502c2f03de9798e160add2d16cb7ced6
MaxiPenduzzu <maximilian.sudo@gmail.com>
2017-02-20 22:08:45 +0100

    css safari bugfix

--- js/css/transitions.js
+++ js/css/transitions.js
@@ -1,30 +1,35 @@
  $(document).ready(function() {
     
     //rotate login_div REGISTER
-    $("#register_option").click(function() {
+    $("#register_option").click(function(e) {
+        e.preventDefault();
         $(".flipper").css('-webkit-transform',"rotateY(180deg)");
     });
      
     //rotate login_div REGISTER -> Back
-     $("#switch_to_login").click(function() {
+     $("#switch_to_login").click(function(e) {
+         e.preventDefault();
          $(".flipper").css('-webkit-transform',"rotateY(360deg)");
      });
      
      //Hamburger Open&Close
-    $(".top_icon_open_sidebar").click(function () {
+    $(".top_icon_open_sidebar").click(function (e) {
+        e.preventDefault();
         $(this).toggleClass("Hide_Hamburger");
         $(".top_icon_close_sidebar").toggleClass("Show_Cross");
         $(".sidebar").toggleClass("Show_Sidebar");
     });
     
     //Open Otions
-    $(".top_icon_settings").click(function () {
+    $(".top_icon_settings").click(function (e) {
+        e.preventDefault();
        $(".top_icon_settings_dropdown").toggleClass("make_visible");
     });
      
      
      //Open add feed
-    $(".add_feed").click(function () {
+    $(".add_feed").click(function (e) {
+        e.preventDefault();
         $(".wrap").toggleClass("url_box_show");
         $(".wrap_url_box").toggleClass("url_box_show");
         $("label").toggleClass("url_box_show");
@@ -33,7 +38,8 @@
      
      
     //pop-up
-    $("div.news_card > table > tbody > tr > td").click(function () {
+    $("div.news_card > table > tbody > tr > td").click(function (e) {
+        e.preventDefault();
         $(".article_popup").toggleClass("article_popup_show");
         $(".background_article_popup").toggleClass("background_article_popup_show");
     }); 
