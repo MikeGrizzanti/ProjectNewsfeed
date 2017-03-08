80b89ec5cab9f92045d35c4f6f8de01808aad60c
MikeGrizzanti <mike.grizzanti@gmail.com>
2017-03-08 14:33:36 +0100

    fnd

--- models/download_feed.inc.php
+++ models/download_feed.inc.php
@@ -93,9 +93,9 @@ if ($_POST) {
                 $query_source_id = DB::getDB()->prepare($sql_source_id);
                 $query_source_id->execute();   
                 $query_source_id->setFetchMode(PDO::FETCH_CLASS, 'tb_source');
-                $query_source_id->fetch();
                 
-                var_dump($query_source_id);
+                
+                var_dump($query_source_id->fetch());
                         
                 
                 $sql_interest = "INSERT INTO tb_user_interests (fk_user_id, fk_interests_id) VALUES (?,?)";
