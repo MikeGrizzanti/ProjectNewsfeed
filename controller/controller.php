edabd680c4358f3424eb2f8808bebba799371cb7
MikeGrizzanti <mike.grizzanti@gmail.com>
2017-04-10 07:39:32 +0100

    changes to user profile template -php

--- controller/controller.php
+++ controller/controller.php
@@ -75,8 +75,9 @@ class controller {
     
     public function user_profile() {
         $erg = is_loggedIn();
-        $this->addContext("error", $error);
         $this->addContext("template", "user_profile");
+        $this->addContext("feed_source", $_SESSION['feed_source']);
+        $this->addContext("category_name", $_SESSION['category_name']);
         if (!$erg) {
             header('Location: index.php?action=login');
         }
