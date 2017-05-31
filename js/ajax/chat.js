5d1421d872cdbcbf584d4b7249f56c5ecd91c8eb
Mike Grizzanti <mike.grizzanti@gmail.com>
2017-05-31 15:13:53 +0200

    pubDate codeerror fixed in ajax

--- js/ajax/chat.js
+++ js/ajax/chat.js
@@ -1,8 +1,31 @@
 $(document).ready(function(e) {
     
     $(".active_chats_container").on("click", function(e){
-        var newsCardId = $(".news_card").attr("id")
+        var newsCardId = $(this).attr("id");
         
+        $.ajax({
+            type: "POST",
+            url:"models/instanceGroupchat.inc.php",
+            data: newsCardId,
+            cache: false,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+                $(".loader").show();
+            },   
+            success: function(data){
+                $(".loader").hide();
+            },
+            error: function (xhr, ajaxOptions, thrownError, url) { //error
+                console.log(xhr.status);
+                console.log(url);
+                alert("There has been a problem while parsing the feed: " + thrownError);
+            },
+            complete: function(data){
+                console.log(data.responseText);
+
+            }
+        }); 
     });
     
 });
